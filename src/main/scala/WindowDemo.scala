c061a8d3615d051f287cffd808d0ffea51d89ca3
Simon <simonchen0317@163.com>
2017-07-17 18:18:47 +0800

    structredStreaming window

--- src/main/scala/WindowDemo.scala
+++ src/main/scala/WindowDemo.scala
@@ -1,4 +1,6 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.execution.streaming.FileStreamSource.Timestamp
+import  org.apache.spark.sql.functions._
 
 object WindowDemo {
   def main(args: Array[String]): Unit = {
@@ -7,21 +9,24 @@ object WindowDemo {
       .appName("StructuredNetworkWordCount")
       .getOrCreate()
 
-    import spark.implicits._
+
     // Create DataFrame representing the stream of input lines from connection to localhost:9999
     val lines = spark.readStream
       .format("socket")
       .option("host", "localhost")
       .option("port", 9999)
       .load()
-
+    import spark.implicits._
     // Split the lines into words
-    val words = lines.as[String].flatMap(_.split(" "))
+//    val words = lines.as[String].flatMap(_.split(" "))
+    val words= lines.as[(String,Timestamp)].flatMap(
+  line=> line._1.split(" ").map(word=>(word, line._2))
+).toDF("word","timestamp")
     // Group the data by window and word and compute the count of each group
     val windowedCounts = words.groupBy(
       window($"timestamp", "10 minutes", "5 minutes"),
       $"word"
-    ).count()
+    ).count().orderBy("window")
 
     // Generate running word count
     val wordCounts = words.groupBy("value").count()
